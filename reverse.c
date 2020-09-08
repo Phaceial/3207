cd4f24cb0e802dbec7773e5d1bc0cf8b38643b1f
Christian Davis <Phaceial@gmail.com>
2020-09-07 20:46:30 -0400

    Issue was with file formatting used terminal instead of vscode to make test files file reading now works too.

--- reverse.c
+++ reverse.c
@@ -1,33 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 256
 
-void reverse (char string[]);
+void reverseInput (char string[]);
 
 int main(int argc, char **argv){
-    char str[BUFFER_SIZE];
+    char string[BUFFER_SIZE];
     FILE *fp = NULL;
 
-
     if (argc == 1){
-        fp = stdin;
         printf("Enter text you would like reversed.\n");
-        fgets(str, sizeof(str), stdin);
-        str[strcspn(str, "\n")] = '\0';
-        reverse(str);
+        fgets(string, sizeof(string), stdin);
+        reverseInput(string);
     }
     else if (argc == 2){
         fp = fopen(argv[1], "r");
+
         if (fp == NULL){
                 fprintf(stderr,"Cannot open file. Check filename/location.\n");
                 exit(1);
             }
-        while ((fgets(str, BUFFER_SIZE, fp)) != NULL){
-            reverse(str);
+        while((fgets(string, sizeof(string), fp)) != NULL){
+            reverseInput(string);
         }
         fclose(fp);
-        
+    }
     // }else if (argc == 3){
     //     strncpy(search, argv[1], sizeof(argv[1]));
     //     for (i = 2; i < argc; i++){
@@ -41,23 +39,23 @@ int main(int argc, char **argv){
     //         }
     //         fclose(fp);
     //     }
-    } else {
+    else {
         fprintf(stderr, "Minimum number of arguments is 1 and maximum is 3");
         exit(1);
     }
     exit(0); 
 }
 
-void reverse(char string[]){
+void reverseInput(char string[]){
     int length = strlen(string);
-    int i = 0;
-    string[strcspn(string, "\n")] = '\0';
+    int i;
+    string[strlen(string)-1] = '\0';
 
-    for (i = length - 1; i >= 0; i--){
-        if(string[i] == ' '){
+      for (i = strlen(string)-1; i >= 0; i--){
+        if(string[i] == ' ') {
             string[i] = '\0';
-            printf("%s ", &string[i] + 1);
+            printf("%s ", &(string[i]) + 1);
         }
     }
-    printf("%s\n", string);
+      printf("%s\n", string);
 }
