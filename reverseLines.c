14c35bf1677e9971cfcc9b1f73cb289c52750546
Christian Davis <Phaceial@gmail.com>
2020-09-08 03:31:13 -0400

    reverse working will put into own method and finish for three lines

--- reverseLines.c
+++ reverseLines.c
@@ -1,11 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 256
 
 int main(int argc, char**argv){
     FILE *fRead = NULL;
     char string[BUFFER_SIZE];
+    char *current = NULL;
+    char **lines = NULL;
+    char **newlines = NULL;
+    size_t buffer;
+    size_t counter = 0;
+    int i;
 
     if(argc == 1){
         printf("Enter text you would like reversed.\n");
@@ -19,25 +25,40 @@ int main(int argc, char**argv){
                 fprintf(stderr,"Cannot open file. Check filename/location.\n");
                 exit(1);
             }
-        while((fgets(string, sizeof(string), fRead)) != NULL){
-            puts(string);
+        while(getline(&current, &buffer, fRead) >= 0){
+            newlines = realloc(lines, (counter+1) * sizeof(*lines));
+            if(newlines == NULL){
+                fprintf(stderr,"Memory has failed to allocate");
+                exit(1);
+            } else lines = newlines;
+            lines[counter] = current;
+            current = NULL;
+            counter++;
         }
+        free(current);
         fclose(fRead);
+
+        for(i = counter - 1; i >=0; i--){
+            printf("%s", lines[i]);
+        }
 
     }else if(argc == 3){
         if(strcmp(argv[1], argv[2]) == 0){
-            fprintf(stderr, "Output file is the same as input");
+            fprintf(stderr, "Output file is the same as input\n");
+            exit(1);
         }
         FILE *fWrite = fopen(argv[2], "w");
         fRead = fopen(argv[1], "r");
-        while((fgets(string, sizeof(string), fRead)) != NULL){
-            fprintf(fWrite, "%s", string);
+        while(getline(&current, &buffer, fRead) >= 0){
+            fprintf(fWrite, "%s", current);
         }
         fclose(fRead);
         fclose(fWrite);
+        free(current);
 
     } else{
-        printf("The minimum arguments is 1 and the maximum arguments are 3");
+        fprintf(stderr,"The minimum arguments is 1 and the maximum arguments are 3\n");
+        exit(1);
     }
-return 0;
+exit(0);
 }
